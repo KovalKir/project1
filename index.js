f73f39fe4b0cdfd9ee16c009544bcd67d57ebcf1
KovalKir <koval.kir2014@yandex.ru>
2022-06-15 15:57:32 +0300

    test1

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 'use strict'
+
+const client = require('prom-client');
+
+const express = require('express');
+
+const exporter = express();
+
+const port = 9200;
+const serverIP = '127.0.0.1';
+
+const registry = new client.Registry();
+
+function setMetricGauge (registry) {
+    
+    const gauge = new client.Gauge({
+        name: 'local_metric',
+        help: 'Local metric description',
+        registers: [registry],
+    });
+
+    function setNumber () { 
+        gauge.set(Math.random()*10);
+       
+    }
+    
+
+    setInterval(setNumber, 5000);
+
+}
+
+
+setMetricGauge(registry);
+
+
+
+exporter.get('/metrics', async (req, res, next) => {
+    res.set('Content-Type', registry.contentType);
+    res.end(registry.metrics());
+    
+    next();
+  });
+
+exporter.listen(port,serverIP, () => console.log('Working'))
+
+
+
+
